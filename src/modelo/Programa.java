99971f9bf127a4124e22ae84de3a663dfc0c0517
achraf <faleh.ach24@triana.salesianos.edu>
2025-11-20 08:14:23 +0100

    he creado un metodo en el clase Persona y creado un objeto en el clase Programa

---
+++ src/modelo/Programa.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Persona p = new Persona("Ana", 20);
+	        p.mostrarDatos();
+
+	}
+
+}
