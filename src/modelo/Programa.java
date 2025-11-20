3c3367818ae5cdc7879fa599301bde3fa7d9c6f0
achraf <faleh.ach24@triana.salesianos.edu>
2025-11-20 09:01:43 +0100

    he cambiado el contenido de los dos clases

--- src/modelo/Programa.java
+++ src/modelo/Programa.java
@@ -5,7 +5,8 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 Persona p = new Persona("Ana", 20);
+		Persona p = new Persona("Ana", 20, "DAM 1");
+
 	        p.mostrarDatos();
 
 	}
